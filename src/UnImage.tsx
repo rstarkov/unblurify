9573fa12fad4591c7e24472656e48e0db6be37c9
Roman Starkov <sourcecode@roman.st>
2021-03-07 17:14:49 +0000

    Fix flash of wrong-sized image by hiding it until fully loaded

--- src/UnImage.tsx
+++ src/UnImage.tsx
@@ -5,26 +5,33 @@ interface UnImageProps {
     url: string;
     zoom: number;
 }
+interface UnImageState {
+    loaded: boolean;
+}
 
-export default class UnImage extends React.Component<UnImageProps, {}>
+export default class UnImage extends React.Component<UnImageProps, UnImageState>
 {
     private imgRef: React.RefObject<HTMLImageElement>;
 
     constructor(props: UnImageProps) {
         super(props);
         this.imgRef = React.createRef();
+        this.state = { loaded: false };
     }
 
     render() {
         return (
             <div style={{ padding: 10, margin: 'auto' }}>
                 <WindowEventNotifier event='resize' onEvent={this.updateImageSize} />
-                <img ref={this.imgRef} src={this.props.url} onLoad={this.handleOnLoad} style={{ display: 'block', imageRendering: this.props.zoom === 1 ? 'auto' : 'pixelated' }} alt="" />
+                <img ref={this.imgRef} src={this.props.url} onLoad={this.handleOnLoad} alt=""
+                    style={{ display: 'block', opacity: this.state.loaded ? 1 : 0, imageRendering: this.props.zoom === 1 ? 'auto' : 'pixelated' }} />
             </div>
         );
     }
 
-    componentDidUpdate(prevProps: UnImageProps, prevState: {}) {
+    componentDidUpdate(prevProps: UnImageProps) {
+        if (prevProps.url != this.props.url)
+            this.setState({ loaded: false });
         this.updateImageSize();
     }
 
@@ -40,5 +47,6 @@ export default class UnImage extends React.Component<UnImageProps, {}>
 
     handleOnLoad = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
         this.updateImageSize();
+        this.setState({ loaded: true });
     }
 }
