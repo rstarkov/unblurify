46de610facc9d971a65da9e3820939f7f21ae2a1
Roman Starkov <sourcecode@roman.st>
2021-03-08 00:28:12 +0000

    Configure wrangler

---
+++ workers-site/index.js
@@ -0,0 +1,58 @@
+import { getAssetFromKV, serveSinglePageApp } from '@cloudflare/kv-asset-handler'
+
+const DEBUG = false
+
+addEventListener('fetch', event => {
+  try {
+    event.respondWith(handleEvent(event))
+  } catch (e) {
+    if (DEBUG) {
+      return event.respondWith(
+        new Response(e.message || e.toString(), {
+          status: 500,
+        }),
+      )
+    }
+    event.respondWith(new Response('Internal Error', { status: 500 }))
+  }
+})
+
+async function handleEvent(event) {
+  const url = new URL(event.request.url)
+  let options = {}
+
+  options.mapRequestToAsset = serveSinglePageApp;
+
+  try {
+    if (DEBUG) {
+      options.cacheControl = {
+        bypassCache: true,
+      }
+    }
+
+    const page = await getAssetFromKV(event, options)
+
+    const response = new Response(page.body, page)
+
+    response.headers.set('X-XSS-Protection', '1; mode=block')
+    response.headers.set('X-Content-Type-Options', 'nosniff')
+    response.headers.set('X-Frame-Options', 'DENY')
+    response.headers.set('Feature-Policy', 'none')
+
+    return response
+
+  } catch (e) {
+
+    if (!DEBUG) {
+      try {
+        let notFoundResponse = await getAssetFromKV(event, {
+          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/index.html`, req),
+        })
+
+        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
+      } catch (e) {}
+    }
+
+    return new Response(e.message || e.toString(), { status: 500 })
+  }
+}
